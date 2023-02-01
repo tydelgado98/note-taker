a8da3ab100419434b7e3ee3f9f97b5ba8ef42361
unknown <delgadojustin100@yahoo.com>
2023-01-31 21:26:18 -0500

    added a direction for the note to go in to

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
-const data = require('../db/db.json');
+let data = require('../db/db.json');
+const {writeFile} = require('fs');
+const { json } = require('express');
+const { restart } = require('nodemon');
 
 router.get('/notes', (req,res) => {
     res.json(data)
+});
+
+router.post('/notes', ({body},res) => {
+    
+    data.push(body);
+    writeFile('./db/db.json', JSON.stringify(data), (err)=>{
+
+        if (err) {console.log(err);};
+        res.json(data);
+      });
+        
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const PORT = process.env.PORT || 3000;
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('public'));
 app.use(require('./routes'));
 
 
