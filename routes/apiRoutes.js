6c312f45ec386779ce80e022a7ab5b815f2036c7
unknown <delgadojustin100@yahoo.com>
2023-01-31 23:06:52 -0500

    officially finished everything up added the delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,12 +9,23 @@ router.get('/notes', (req,res) => {
 });
 
 router.post('/notes', ({body},res) => {
-    
+    body.id = Math.floor(Math.random()*1000000).toString(16);
     data.push(body);
     writeFile('./db/db.json', JSON.stringify(data), (err)=>{
 
         if (err) {console.log(err);};
         res.json(data);
+      });
+        
+});
+
+router.delete('/notes/:id', ({params},res) => {
+    
+    let newData = data.filter(obj => obj.id != params.id );
+    writeFile('./db/db.json', JSON.stringify(newData), (err)=>{
+
+        if (err) {console.log(err);};
+        res.send('done!');
       });
         
 });
